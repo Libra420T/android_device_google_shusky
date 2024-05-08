c0dc3babcff2c684a3fd17714dd3b11e3405dd6f
Austin Young <austineyoung2000@gmail.com>
2024-05-08 03:35:50 -0400

    shusky: Override system properties
    Change-Id: I9ac15c131b480dc568df1dac310b039095432e74

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,7 @@
 # SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
+
+DEVICE_PATH := device/google/shusky
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_shusky
