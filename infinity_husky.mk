2cdd7ad12f75bf1808c570b300a63f4df3f585bd
Libra420T <ryantf420@gmail.com>
2025-04-06 13:42:56 -0400

    Shusky: Adapt for Infinity

--- infinity_husky.mk
+++ infinity_husky.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := husky
@@ -19,9 +19,31 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := infinity_$(DEVICE_CODENAME)
+
+# Infinity Build Flags
+INFINITY_MAINTAINER := Libra420T
+INFINITY_BUILD_TYPE := UNOFFICIAL
+
+# Blur
+TARGET_SUPPORTS_BLUR := true
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# GMS
+WITH_GMS := true
+WITH_GAPPS := true
+TARGET_BUILD_GOOGLE_TELEPHONY := true
+
+# Aperture
+PRODUCT_NO_CAMERA := true
+# AudioFX
+TARGET_SHIPS_AUDIOFX := false
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SCREEN_HEIGHT := 2992
 TARGET_SCREEN_WIDTH := 1344
 
