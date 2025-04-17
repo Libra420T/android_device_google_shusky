6d184832a9facfbda0f5b6c52afacf04b2f852f7
Libra420T <ryantf420@gmail.com>
2025-04-17 18:45:41 -0400

    Shusky: Nuke Debug Override

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,9 +6,6 @@
 
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_shusky
-
 # Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_kernel_boot))
